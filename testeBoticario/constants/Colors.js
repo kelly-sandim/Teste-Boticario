a9bdd3d39f337ea496bbfb9e76e96e3129f03239
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-04-18 19:38:13 -0400

    Criado esqueleto do código com o Expo

---
+++ testeBoticario/constants/Colors.js
@@ -0,0 +1,14 @@
+const tintColor = '#E53935';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#E53935',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
