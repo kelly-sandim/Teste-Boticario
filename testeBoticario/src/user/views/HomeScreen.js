7adf85c2db83c92339a278cb1e0c7efdc2575870
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-04-20 16:59:00 -0400

    Tive de reinstalar todas as libs de novo porque o Expo bugou

--- testeBoticario/src/user/views/HomeScreen.js
+++ testeBoticario/src/user/views/HomeScreen.js
@@ -1,54 +1,238 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, 
+        Platform, 
+        StyleSheet, 
+        Text, 
+        FlatList,
+        TouchableHighlight,
+        TouchableOpacity, 
+        View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import {
+  Container,
+  Header,
+  Body,
+  Content,
+  Left,
+  Title,
+  Thumbnail,
+  Col,
+  Row,
+  Grid,
+  Icon,
+  Spinner,
+  Fab,
+  Button,
+  Footer,
+  Input,
+  Right
+} from "native-base";
+
+import { connect } from "react-redux";
+
 
 import { MonoText } from '../../../components/StyledText';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../../../assets/images/robot-dev.png')
-                : require('../../../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+    <Container>
+        {this.props.newTweetModalOpen && Platform.OS === "android" ? null : (
+          <Header style={styles.topMargin}>
+            <Left>
+              <Thumbnail small source={{ uri: this.props.user.avatar }} />
+            </Left>
+            <Body>
+              <Title style={{ color: "#121212" }}>Home</Title>
+            </Body>
+            <Right>
+              <Button transparent onPress={this.openModal.bind(this)}>
+                <Icon name="md-create" style={{ color: "#4286f4" }} />
+              </Button>
+            </Right>
+          </Header>
+        )}
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
+        <Modal
+          ref={"newTweetModal"}
+          backdrop={true}
+          style={styles.modal}
+          isOpen={this.props.newTweetModalOpen}
+          onClosed={this.closeModal.bind(this)}
+        >
+          <View
+            style={{
+              alignSelf: "flex-start",
+              alignItems: "center",
+              flexDirection: "row",
+              padding: 5,
+              paddingRight: 10
+            }}
+          >
+            <Button transparent onPress={this.closeModal.bind(this)}>
+              <Icon name="close" style={{ color: "black", fontSize: 32 }} />
+            </Button>
+            <View style={{ flex: 1 }} />
+            <Thumbnail
+              small
+              source={{
+                uri:
+                  "https://i1.wallpaperscraft.ru/image/betmen_art_minimalizm_107658_300x240.jpg"
+              }}
+            />
           </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              width: "100%"
+            }}
+          >
+            <Input
+              style={{
+                flex: 1,
+                width: "100%",
+                fontSize: 24,
+                alignContent: "flex-start",
+                justifyContent: "flex-start",
+                textAlignVertical: "top",
+                margin: 5
+              }}
+              multiline
+              placeholder="What's happening?"
+              onChangeText={tweet => this.setState({ newTweetContent: tweet })}
+            />
+          </View>
+          <View style={styles.modalFooter}>
+            <Button transparent small>
+              <Icon name="ios-image" />
+            </Button>
+            <Button transparent small>
+              <Icon name="ios-pin" />
+            </Button>
+            <Button transparent small>
+              <Icon name="ios-stats-outline" />
+            </Button>
 
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
-          </Text>
+            <View style={{ flex: 1 }} />
+            {this.props.tweetPosted === "ongoing" ? <Spinner /> : null}
+            <Button
+              rounded
+              style={{ color: "#4286f4", height: 40, width: 94 }}
+              onPress={this.postTweet.bind(this)}
+            >
+              <Text style={{ color: "white" }}>Tweet</Text>
+            </Button>
+          </View>
+        </Modal>
+        <Content style={{ backgroundColor: "white" }}>
+          {this.props.fetchingTweets ? (
+            <View
+              contentContainerStyle={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              <Spinner color="blue" />
+            </View>
+          ) : (
+            <View style={{ justifyContent: "flex-start" }}>
+              <FlatList
+                data={this.props.tweets}
+                keyExtractor={this._keyExtractor}
+                renderItem={({ item }) => (
+                  <View style={styles.tweet}>
+                    <TouchableHighlight
+                      onPress={this._profileClick.bind(this, item.user)}
+                      underlayColor="white"
+                      activeOpacity={0.75}
+                    >
+                      <View style={{ flex: 1, flexDirection: "row" }}>
+                        <Thumbnail source={{ uri: item.user.avatar }} />
+                        <View
+                          style={{
+                            flexDirection: "column",
+                            justifyContent: "flex-start"
+                          }}
+                        >
+                          <Text
+                            style={{
+                              paddingLeft: 15,
+                              fontWeight: "bold",
+                              fontSize: 20
+                            }}
+                          >
+                            {item.user.name}
+                          </Text>
+
+                          <Text
+                            style={{
+                              paddingLeft: 15,
+                              color: "#aaa",
+                              fontSize: 16
+                            }}
+                          >
+                            {"@" + item.user.username}
+                          </Text>
+                        </View>
+                      </View>
+                    </TouchableHighlight>
+                    <Text style={styles.tweetText}>{item.tweetContent}</Text>
+                    <View style={styles.tweetFooter}>
+                      <View style={styles.footerIcons}>
+                        <Button
+                          transparent
+                          dark
+                          onPress={this._tweetDetails.bind(this, item)}
+                        >
+                          <Icon name="ios-text-outline" />
+                          <Text style={styles.badgeCount}>{item.replies}</Text>
+                        </Button>
+                      </View>
+                      <View style={styles.footerIcons}>
+                        <Button transparent dark>
+                          <Icon name="ios-repeat" />
+                          <Text style={styles.badgeCount}>{item.retweets}</Text>
+                        </Button>
+                      </View>
+                      <View style={styles.footerIcons}>
+                        <Button transparent dark>
+                          <Icon name="ios-heart-outline" />
+                          <Text style={styles.badgeCount}>{item.likes}</Text>
+                        </Button>
+                      </View>
+                      <View style={styles.footerIcons}>
+                        <Button transparent dark>
+                          <Icon name="ios-mail-outline" />
+                        </Button>
+                      </View>
+                    </View>
+                  </View>
+                )}
+              />
+              {this.state.newTweetModalOpen ? null : (
+                <Fab
+                  position="bottomRight"
+                  style={{ backgroundColor: "#4286f4", zIndex: -1 }}
+                  onPress={this.openModal.bind(this)}
+                  ref={"FAB"}
+                >
+                  <Icon name="md-create" />
+                </Fab>
+              )}
+            </View>
+          )}
+        </Content>
+
+        {/* <View tabLabel="Search">
+          <Text>Search</Text>
         </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-        </View>
-      </View>
-    </View>
+        <View tabLabel="Messages">
+          <Text>Messages</Text>
+        </View> */}
+      </Container>
   );
 }
 
